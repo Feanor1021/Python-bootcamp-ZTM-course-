cd4c444eb1210e7a41f1b350aae92bac13f8c3f4
Feanor1021 <furkanyardimci1006@gmail.com>
2023-01-16 21:04:33 +0300

    augmented assignment

---
+++ 2.Operators_and_Math_functions/augmented_assignment.py
@@ -0,0 +1,18 @@
+
+some_value = 15
+
+some_value +=2
+print(some_value)
+
+some_value -=2
+print(some_value)
+
+some_value *=2
+print(some_value)
+
+some_value /=2
+print(some_value)
+
+#you can use most of the operators like this as in C.(even the bitwise ones : <<, >>, |, &, ^ ...).
+#detailed information is in this link https://www.geeksforgeeks.org/augmented-assignment-operators-in-python/
+
