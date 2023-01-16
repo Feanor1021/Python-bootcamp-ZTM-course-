da26be933482a25532cd98d38796ffeb9d519089
Furkan Yardımcı <92301220+Feanor1021@users.noreply.github.com>
2023-01-17 01:29:27 +0300

    Delete String_op_escape_characters.py

--- 3.Strings/String_op_escape_characters.py
+++
@@ -1,8 +0,0 @@
-string1="hello"
-string2="world"
-
-print(string1+string2) #Concatenation - Adds values on either side of the operator. values are must be strings
-print(string1*2) #Repetition - Creates new strings, concatenating multiple copies of the same string
-print('h' in string1) #Membership - Returns true if a character exists in the given string
-print("5" not in string1) #Membership - Returns true if a character does not exist in the given string
-print(r"\n\n"+string1) #Raw String - Suppresses actual meaning of Escape characters.
