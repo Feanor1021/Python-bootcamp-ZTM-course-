389fa26522da473de0a108846e2783e5720d656d
Feanor1021 <furkanyardimci1006@gmail.com>
2023-01-21 00:35:49 +0300

    Simple exercise about for loops

--- 6.Loops/Exercise1.py
+++ 6.Loops/Exercise1.py
@@ -1,9 +1,9 @@
 
-my_list = list((1,2,3,4,51,12,5,7,23,124,13213,"eqwqw",r"\n\n"))
+my_list = list((1,2,3,4,51,12,5,7,23,124,13213))
 counter = 0
 
 for i in my_list:
-    counter += 1
+    counter += i
     print(counter,i)
 
 # for i in list(range(1,51)):
